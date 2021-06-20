6a7e120872bac60e39a421c7575e9c4de9c6d5d5
Raul Fernandez <raul.fernandez@qubiq.es>
2021-06-21 00:31:59 +0200

    [UPD] Upload module

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Dialogflow Connector",
+
+    'summary': """Connect Dialogflow with Odoo ERP""",
+
+    'description': """
+        This module connect dialogflow with odoo and create events and opportunities
+    """,
+    "category": "Tools",
+    "website": "https://github.com/rauferdeveloper",
+    "author": "Raúl Fernández",
+    "license": "LGPL-3",
+    'depends': [
+        'base',
+        'contacts',
+        'calendar',
+        'crm'
+    ],
+    'data': [
+        'data/settings.xml'
+
+    ],
+    'application': True,
+    "installable": True,
+}
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from . import calendar_event
+from . import res_partner
+from . import crm_lead
